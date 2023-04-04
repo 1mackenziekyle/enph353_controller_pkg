de93f5e1eb1983dee99ef7618293b3b6e9567269
Kyle Mackenzie <1mackenziekyle@gmail.com>
2023-04-04 00:02:53 -0700

    Testing models

--- src/controller.py
+++ src/controller.py
@@ -59,13 +59,13 @@ SHOW_MODEL_OUTPUTS = False
 DEBUG_HSV_OUTPUT = False
 DEBUG_LISENCE_MASK = False
 # ========== Saving images
-IMAGE_SAVE_FOLDER = 'images/inner_lap/first/base10000'
+IMAGE_SAVE_FOLDER = 'images/outer_lap/final/saddle7'
 SNAPSHOT_FREQUENCY = 2
 COLOR_CONVERTER = cv2.COLOR_BGR2GRAY
 RESIZE_FACTOR = 20
 
 # ========== Operating 
-OPERATING_MODE = Operating_Mode.MODEL
+OPERATING_MODE = Operating_Mode.MANUAL
 TEST_INNER_LOOP = False
 
 # ========== Model Settings
@@ -73,6 +73,7 @@ OUTER_LOOP_LINEAR_SPEED = 0.3645
 OUTER_LOOP_ANGULAR_SPEED = 1.21
 INNER_LOOP_LINEAR_SPEED = 0.266
 INNER_LOOP_ANGULAR_SPEED = 1.0
+# OUTER_LOOP_DRIVING_MODEL_PATH = 'models/outer_lap/5convlayers/final/saddle6'
 OUTER_LOOP_DRIVING_MODEL_PATH = 'models/outer_lap/5convlayers/final/saddle6'
 INNER_LOOP_DRIVING_MODEL_PATH = 'models/inner_lap/first/base10000'
 
@@ -130,7 +131,7 @@ class Controller:
         print('=== state: ', self.state)
         # Jump to state
         self.RunCurrentState()
-        self.label_license_plate(self.camera_feed)
+        # self.label_license_plate(self.camera_feed)
         self.show_camera_feed(self.camera_feed)
         # TODO: REMOVE
         time.sleep(0.04)
