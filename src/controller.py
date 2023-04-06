12a6e28d58a95e0746b27b7b73dd58ceed3d2c44
Kyle Mackenzie <1mackenziekyle@gmail.com>
2023-04-06 13:47:16 -0700

    on faster/saddle2

--- src/controller.py
+++ src/controller.py
@@ -57,7 +57,7 @@ SHOW_MODEL_OUTPUTS = False
 DEBUG_HSV_OUTPUT = False
 DEBUG_LISENCE_MASK = False
 # ========== Saving images
-IMAGE_SAVE_FOLDER = 'images/outer_lap/faster/base10000'
+IMAGE_SAVE_FOLDER = 'images/outer_lap/faster/saddle2'
 SNAPSHOT_FREQUENCY = 2
 COLOR_CONVERTER = cv2.COLOR_BGR2GRAY
 RESIZE_FACTOR = 20
@@ -73,7 +73,7 @@ OUTER_LOOP_LINEAR_SPEED = 0.5
 OUTER_LOOP_ANGULAR_SPEED = 2.14 
 INNER_LOOP_LINEAR_SPEED = 0.266
 INNER_LOOP_ANGULAR_SPEED = 1.0
-OUTER_LOOP_DRIVING_MODEL_PATH = 'models/outer_lap/5convlayers/faster/base10000'
+OUTER_LOOP_DRIVING_MODEL_PATH = 'models/outer_lap/5convlayers/faster/saddle2'
 INNER_LOOP_DRIVING_MODEL_PATH = 'models/inner_lap/first/base10000'
 
 
